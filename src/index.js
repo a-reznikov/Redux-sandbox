425bf1f0a48ae1b085b9189ee33159ba263d4573
a-reznikov <a.reznnikov@gmail.com>
2023-10-11 12:04:41 +0300

    feat: refactor

--- src/index.js
+++ src/index.js
@@ -2,31 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import { createStore } from 'redux' //! Old
 import { configureStore } from '@reduxjs/toolkit';
+import reducer from './reducer';
+import { inc, dec, rnd } from './actions';
 
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render(
 //     <h1>Start</h1>
 // );
 
-const reducer = (state = 0, action) => {
-  switch (action.type) {
-    case 'INC':
-      return ++state;
-    case 'DEC':
-      return --state;
-    case 'RND':
-      return state + action.payload;
-    default:
-      return state;
-  }
-};
-
 // const store = createStore(reducer); //! Old
 const store = configureStore({ reducer });
-
-const inc = () => ({ type: 'INC' })
-const dec = () => ({ type: 'DEC' })
-const rnd = (payload) => ({ type: 'RND', payload })
 
 const update = () => {
   document.getElementById('counter').textContent = store.getState();
